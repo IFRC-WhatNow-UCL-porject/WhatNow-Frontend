af229c4fb9c7018bf19c20f6d2b9b35a655e1f3e
ROWANMENG <mqy2021official@163.com>
2024-03-19 11:16:52 +0000

    add deployed server

--- src/services/base.js
+++ src/services/base.js
@@ -1,3 +1,7 @@
-const BASE_URL = 'http://localhost:5000';
+var BASE_URL = 'http://localhost:5000';
+
+if (process.env.NODE_ENV === 'production') {
+    BASE_URL = 'https://api-preparecenter.azurewebsites.net';
+}
 
 export default BASE_URL;
